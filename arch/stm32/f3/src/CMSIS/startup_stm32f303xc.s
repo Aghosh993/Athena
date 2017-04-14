16252a76beb1b72cab828b6e332aceded70e0c29
Aghosh993 <Aghosh993@gmail.com>
2017-04-14 10:13:53 -0400

    Updating big-endian toolchain and fixing TMS570 makefile.

--- arch/stm32/f3/src/CMSIS/startup_stm32f303xc.s
+++ arch/stm32/f3/src/CMSIS/startup_stm32f303xc.s
@@ -76,7 +76,7 @@ defined in linker script */
 	.weak	Reset_Handler
 	.type	Reset_Handler, %function
 Reset_Handler:
-  ldr   sp, =_estack    /* Atollic update: set stack pointer */
+/*  ldr   sp, =_estack  */  /* Atollic update: set stack pointer */
 
 /* Copy the data segment initializers from flash to SRAM */
   movs	r1, #0
@@ -109,7 +109,7 @@ LoopFillZerobss:
 /* Call the clock system intitialization function.*/
     bl  SystemInit
 /* Call static constructors */ 
-    bl __libc_init_array
+/* bl __libc_init_array */
 /* Call the application's entry point.*/
 	bl	main
 
